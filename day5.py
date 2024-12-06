f178960e0bfb5391e24125ff8ba8114ea77ee1c5
Rob Harrigan <rob.harrigan89@gmail.com>
2024-12-06 00:17:42 -0600

    Days 5 and 6

---
+++ day5.py
@@ -0,0 +1,161 @@
+from collections import defaultdict
+
+from rich import print
+
+from aoc.files import readlines
+
+data = readlines(5)
+data = [line.strip() for line in data]
+
+sample = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+""".split("\n")
+# Uncomment this line to use the sample data
+# data = sample
+
+# Part 1
+ordering = []
+updates = []
+for line in data:
+    if "|" in line:
+        ordering.append(line)
+    elif "" == line:
+        continue
+    else:
+        updates.append(line)
+ordering = [[int(i) for i in line.split("|")] for line in ordering]
+ordering_dict = defaultdict(list)
+for order in ordering:
+    ordering_dict[order[0]].append(order[1])
+updates = [[int(i) for i in line.split(",")] for line in updates]
+
+# Start by identifying which updates are in the right order
+update_is_ordered = [True] * len(updates)
+for idx, update in enumerate(updates):
+    # print(f"\nStarting on update: {update}")
+    for i in range(1, len(update)):
+        current_page = update[i]
+        ordering_rules = [True] * len(ordering_dict[current_page])
+        # print(f"{current_page=} {ordering_dict[current_page]=}")
+        for ordering_idx, after_page in enumerate(ordering_dict[current_page]):
+            try:
+                after_idx = update.index(after_page)
+            except ValueError:
+                continue
+            # print(f"{current_page=} {after_page=} {after_idx=} {i=} {after_idx < i=}")
+            if after_idx < i:
+                update_is_ordered[idx] = False
+                continue
+# for update, is_ord in zip(updates, update_is_ordered):
+#     print(f"{update=} {is_ord=}")
+print(f"Part 1: {sum(update_is_ordered)=} / {len(update_is_ordered)=}")
+# Now find the middle page number for each update that is correctly ordered
+mp = 0
+for idx, update in enumerate(updates):
+    if update_is_ordered[idx]:
+        # ordered_update = sorted(update)
+        length = len(update)
+        if length % 2 == 0:
+            # print(f"{update=} {length=} {length // 2 - 1=}")
+            mp += update[length // 2 - 1]
+        else:
+            # print(f"{update=} {length=} {length // 2=}")
+            mp += update[length // 2]
+print(f"Part 1: {mp}")
+
+
+# Part 2
+# For each of the incorrectly-ordered updates, use the page ordering rules to put the numbers in the right order
+# Then find the middle page number and add them
+def is_ordered(update):
+    for i in range(1, len(update)):
+        current_page = update[i]
+        # print(f"{current_page=} {ordering_dict[current_page]=}")
+        for after_page in ordering_dict[current_page]:
+            try:
+                after_idx = update.index(after_page)
+            except ValueError:
+                continue
+            # print(f"{current_page=} {after_page=} {after_idx=} {i=} {after_idx < i=}")
+            if after_idx < i:
+                return False
+    return True
+
+
+def move_before(update, current_page, before_page):
+    before_idx = update.index(before_page)
+    current_idx = update.index(current_page)
+    # Insert the current page before the before page
+    update.pop(current_idx)
+    update.insert(before_idx, current_page)
+
+
+def move_after(update, current_page, after_page):
+    # Move the after page to one after the current_page
+    after_idx = update.index(after_page)
+    current_idx = update.index(current_page)
+    # Insert the after page after the current page
+    update.pop(after_idx)
+    update.insert(current_idx + 1, after_page)
+
+
+mp = 0
+for idx, update in enumerate(updates):
+    if not update_is_ordered[idx]:
+        # print(f"\nStarting on update: {update}")
+        while not is_ordered(update):
+            for i in range(len(update)):
+                current_page = update[i]
+                rules = ordering_dict[current_page]
+                # print(f"{i=} {current_page=} {rules=}")
+                # If none of the rules apply, skip
+                if not any([after_page in update for after_page in rules]):
+                    # print(f"Skipping {current_page=} {rules=}")
+                    continue
+                for after_page in rules:
+                    try:
+                        after_idx = update.index(after_page)
+                    except ValueError:
+                        continue
+                    # print(f"{current_page=} {after_page=} {after_idx=} {i=} {after_idx < i=}")
+                    if after_idx < i:
+                        # Rule is violated
+                        move_after(update, current_page, after_page)
+        if not is_ordered(update):
+            print(f"Update is still not ordered: {update}")
+        # print(f"Ordered update: {update} {is_ordered(update)=}")
+        length = len(update)
+        if length % 2 == 0:
+            mp += update[length // 2 - 1]
+            # print(f"{update=} {length=} {length // 2 - 1=}")
+        else:
+            mp += update[length // 2]
+            # print(f"{update=} {mp=} {length=} {length // 2=}")
+print(f"Part 2: {mp}")
---
+++ day6.py
@@ -0,0 +1,213 @@
+from collections import Counter
+from copy import deepcopy
+
+from rich import print
+from tqdm import tqdm
+
+from aoc.files import readlines
+
+data = readlines(6)
+
+GU = "^"
+GR = ">"
+GD = "V"
+GL = "<"
+OBS = "#"
+MARK = "X"
+# The guard moves up until they hit an obstacle, turn right 90 degrees, continue
+# Count the number of distinct positions on the map they visit
+
+sample = """
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+""".split("\n")
+# data = sample
+
+
+def replace_char(s, i, c):
+    return s[:i] + c + s[i + 1 :]
+
+
+def move_guard(data):
+    gy = [(GU in l or GR in l or GD in l or GL in l) for l in data].index(True)
+    # Find out which guard is in the row
+    if GU in data[gy]:
+        gx = data[gy].index(GU)
+        new_gy = gy - 1
+        new_gx = gx
+    elif GR in data[gy]:
+        gx = data[gy].index(GR)
+        new_gy = gy
+        new_gx = gx + 1
+    elif GD in data[gy]:
+        gx = data[gy].index(GD)
+        new_gy = gy + 1
+        new_gx = gx
+    elif GL in data[gy]:
+        gx = data[gy].index(GL)
+        new_gy = gy
+        new_gx = gx - 1
+
+    if data[new_gy][new_gx] == OBS:
+        # Turn right and don't move
+        if data[gy][gx] == GU:
+            data[gy] = replace_char(data[gy], gx, GR)
+        elif data[gy][gx] == GR:
+            data[gy] = replace_char(data[gy], gx, GD)
+        elif data[gy][gx] == GD:
+            data[gy] = replace_char(data[gy], gx, GL)
+        elif data[gy][gx] == GL:
+            data[gy] = replace_char(data[gy], gx, GU)
+    else:
+        curr_g = data[gy][gx]
+        data[new_gy] = replace_char(data[new_gy], new_gx, curr_g)
+        data[gy] = replace_char(data[gy], gx, MARK)
+    return data
+
+
+# print("\n".join(data))
+while True:
+    try:
+        data = move_guard(data)
+    except IndexError:
+        # Replace the guard with a mark
+        gy = [(GU in l or GR in l or GD in l or GL in l) for l in data].index(True)
+        if GU in data[gy]:
+            gx = data[gy].index(GU)
+        elif GR in data[gy]:
+            gx = data[gy].index(GR)
+        elif GD in data[gy]:
+            gx = data[gy].index(GD)
+        elif GL in data[gy]:
+            gx = data[gy].index(GL)
+        data[gy] = replace_char(data[gy], gx, MARK)
+        break
+
+# print("\n".join(data))
+print(sum([l.count(MARK) for l in data]))
+
+
+# Part 2
+# Now we want to get the guard caught in a loop by placing a SINGLE obstruction
+# Find all of the possible places to put the obstruction such that the guard gets caught in a loop
+
+
+def get_guard_post(data):
+    gy = [(GU in l or GR in l or GD in l or GL in l) for l in data].index(True)
+    if GU in data[gy]:
+        gx = data[gy].index(GU)
+    elif GR in data[gy]:
+        gx = data[gy].index(GR)
+    elif GD in data[gy]:
+        gx = data[gy].index(GD)
+    elif GL in data[gy]:
+        gx = data[gy].index(GL)
+    return gx, gy
+
+
+def is_obs_adjacent(data, gxy):
+    gx, gy = gxy
+    # Handle boundaries
+    maxy = len(data) - 1
+    maxx = len(data[0]) - 1
+    if gy == 0 or gy == maxy or gx == 0 or gx == maxx:
+        return False
+    if gy > 0 and data[gy - 1][gx] == OBS:
+        return True
+    if gy < maxy and data[gy + 1][gx] == OBS:
+        return True
+    if gx > 0 and data[gy][gx - 1] == OBS:
+        return True
+    if gx < maxx and data[gy][gx + 1] == OBS:
+        return True
+    return False
+
+
+def move_guard(data, gxy):
+    gx, gy = gxy
+    if GU in data[gy]:
+        new_gy = gy - 1
+        new_gx = gx
+    elif GR in data[gy]:
+        new_gy = gy
+        new_gx = gx + 1
+    elif GD in data[gy]:
+        new_gy = gy + 1
+        new_gx = gx
+    elif GL in data[gy]:
+        new_gy = gy
+        new_gx = gx - 1
+    else:
+        raise ValueError("Guard not found")
+
+    if is_oob((new_gx, new_gy)):
+        # If we've reached an edge of the map just return
+        return data, (new_gx, new_gy), False
+
+    if data[new_gy][new_gx] == OBS:
+        # Turn right and don't move
+        new_gx, new_gy = gx, gy
+        turned = True
+        if data[gy][gx] == GU:
+            data[gy] = replace_char(data[gy], gx, GR)
+        elif data[gy][gx] == GR:
+            data[gy] = replace_char(data[gy], gx, GD)
+        elif data[gy][gx] == GD:
+            data[gy] = replace_char(data[gy], gx, GL)
+        elif data[gy][gx] == GL:
+            data[gy] = replace_char(data[gy], gx, GU)
+    else:
+        turned = False
+        curr_g = data[gy][gx]
+        data[new_gy] = replace_char(data[new_gy], new_gx, curr_g)
+        data[gy] = replace_char(data[gy], gx, MARK)
+    return data, (new_gx, new_gy), turned
+
+
+def is_oob(gxy):
+    gx, gy = gxy
+    if gy < 0 or gy >= len(data) or gx < 0 or gx >= len(data[0]):
+        return True
+    return False
+
+
+def is_looped(start_data):
+    # Check if the guard gets caught in a loop
+    # We know this happens if the guard repeats a turn
+    i = 0
+    gxys = Counter()
+    gxy = get_guard_post(start_data)
+    while True:
+        start_data, gxy, turned = move_guard(start_data, gxy)
+        if is_oob(gxy):
+            return False
+        if turned:
+            gxys[gxy] += 1
+            if gxys[gxy] > 5:
+                return True
+        i += 1
+        if i > 100_000:
+            print("WARNING: Too many iterations reached")
+            return True
+
+
+data = readlines(6)
+cnt = 0
+for y in tqdm(range(len(data))):
+    # print(f"Checking row {y}, current count: {cnt}")
+    for x in range(len(data[0])):
+        if data[y][x] == OBS or data[y][x] in [GU, GR, GD, GL]:
+            continue
+        new_data = deepcopy(data)
+        new_data[y] = replace_char(new_data[y], x, OBS)
+        if is_looped(new_data):
+            cnt += 1
+print(cnt)
