0d2ec852e261c47c3ec24e96f692ebed2592dff3
Rob Harrigan <Rob.Harrigan89@gmail.com>
2024-12-16 11:51:38 -0600

    Finally adding a Point class makes logic much more readable

--- aoc/grid.py
+++ aoc/grid.py
@@ -1,3 +1,5 @@
+import math
+from functools import total_ordering
 from rich import print
 
 
@@ -58,3 +60,69 @@ def print_grid_pt(grid, pt):
             else:
                 print(cell, end="")
         print()
+
+
+@total_ordering
+class Point:
+    """Simple 2-dimensional point."""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, n):
+        return Point(self.x * n, self.y * n)
+
+    def __div__(self, n):
+        return Point(self.x / n, self.y / n)
+
+    def __neg__(self):
+        return Point(-self.x, -self.y)
+
+    def __eq__(self, other):
+        if type(other) != Point:
+            return False
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __lt__(self, other):
+        return self.length < other.length
+
+    def __invert__(self):
+        return Point(-self.y, -self.x)
+
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+
+    def __repr__(self):
+        return "Point({}, {})".format(self.x, self.y)
+
+    def __hash__(self):
+        return hash(tuple((self.x, self.y)))
+
+    def __getitem__(self, idx):
+        if idx == 0:
+            return self.x
+        if idx == 1:
+            return self.y
+        raise IndexError
+
+    @property
+    def length(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+
+DIRS = [
+    Point(0, 1),  # north
+    Point(1, 0),  # east
+    Point(0, -1),  # south
+    Point(-1, 0),  # west
+]
--- day16.py
+++ day16.py
@@ -1,60 +1,30 @@
 import heapq
 import re
-from collections import Counter, defaultdict
+from collections import defaultdict
 from copy import deepcopy
 
 from rich import print
 
 from aoc.files import readlines
 from aoc.pr import pr
+from aoc.grid import Point, DIRS
 
 
-def min_path_grid_with_turns(grid, start):
-    """
-    Finds the minimum path between two points in a grid, considering turn costs.
-
-    Args:
-        grid: A 2D list representing the grid (costs).
-        start: A tuple (row, col) representing the start point.
-        end: A tuple (row, col) representing the end point.
-        turn_cost: The cost incurred for each turn.
-
-    Returns:
-        A tuple: (min_cost, path) or (inf, None) if no path exists.
-    """
-
+def min_path_grid_with_turns(grid, start, dirs):
     maxy = len(grid)
     maxx = len(grid[0])
 
     distances = defaultdict(list)  # Key: (x, y, direction)
     previous = defaultdict(list)  # Key: (x, y, direction)
     priority_queue = [(0, start, "R")]  # (distance, (x, y), previous_direction)
+    ptsible_moves = [(v[0], v[1], k) for k, v in dirs.items()]
 
     while priority_queue:
         current_distance, current_node, prev_dir = heapq.heappop(priority_queue)
 
-        # if (current_node, prev_dir) in distances and current_distance > distances[
-        #     (current_node, prev_dir)
-        # ]:
-        #     continue
-        # if current_node == end:
-        #     path = []
-        #     node_dir = (current_node, prev_dir)
-        #     while node_dir:
-        #         node, direction = node_dir
-        #         path.insert(0, node)
-        #         node_dir = previous.get(node_dir)
-        #     return current_distance, path
-
         x, y = current_node
-        possible_moves = [
-            (-1, 0, "L"),
-            (1, 0, "R"),
-            (0, -1, "U"),
-            (0, 1, "D"),
-        ]  # (dx, dy, direction)
 
-        for dx, dy, direction in possible_moves:
+        for dx, dy, direction in ptsible_moves:
             nx, ny = x + dx, y + dy
             if grid[ny][nx] == WALL:
                 continue
@@ -62,12 +32,13 @@ def min_path_grid_with_turns(grid, start):
                 new_distance = current_distance + 1
                 if prev_dir and prev_dir != direction:  # Check for turn
                     new_distance += 1000  # Turn cost
-                # if (x, y) == (3, 7) or (x, y) == (13, 1):
-                #     print(
-                #         f"Checking {nx, ny} {grid[ny][nx]} Coming from {(x, y)} {prev_dir} ({current_distance})->({new_distance})"
-                #     )
-                #     print(distances.get(((nx, ny), direction), float("inf")))
                 if grid[ny][nx] == END:
+                    print(
+                        f"Found end at {nx, ny} {prev_dir}/{direction} {new_distance}"
+                    )
+                    print(
+                        f"Coming from node: {current_node} {direction} which has cost {current_distance}"
+                    )
                     distances[((nx, ny), direction)].append(new_distance)
                     previous[((nx, ny), direction)].append((current_node, prev_dir))
                     continue
@@ -108,8 +79,26 @@ if __name__ == "__main__":
 #S..#.....#...#
 ###############
 """.strip().splitlines()
+    sample = """
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################""".strip().splitlines()
     # Comment out this line to use actual data
-    data = sample
+    # data = sample
 
     dirs = {
         "L": (-1, 0),
@@ -133,7 +122,7 @@ if __name__ == "__main__":
 
     grid = [list(row) for row in data]
     start, end = find_start_end(grid)
-    distances, previous = min_path_grid_with_turns(grid, start)
+    distances, previous = min_path_grid_with_turns(grid, start, dirs)
 
     min_cost = float("inf")
     for direction in ["U", "D", "L", "R"]:
@@ -141,89 +130,53 @@ if __name__ == "__main__":
             cost = min(distances[(end, direction)])
             if cost < min_cost:
                 min_cost = cost
+                last_dir = direction
     pr(min_cost)
     # Find all paths that have the minimum cost
     print("Part 2")
-    seen = set()
 
-    def get_paths_from_node(node, start, seen=seen, prev_dir=None):
-        path = [node]
-        dists = {}  # Key: node value: distance
-        for dir in ["U", "D", "L", "R"]:
-            dx, dy = dirs[dir]
-            prev_node = (node[0] - dx, node[1] - dy)
-            if not (0 <= prev_node[0] < len(grid[0]) and 0 <= prev_node[1] < len(grid)):
+    BOARD = {}
+    for y, row in enumerate(grid):
+        for x, c in enumerate(row):
+            p = Point(x, y)
+            BOARD[p] = WALL if c == WALL else EMPTY
+            if c == START:
+                START = p
+            elif c == END:
+                END = p
+
+    start = Point(start[0], start[1])
+    end = Point(end[0], end[1])
+    todo = [(0, start, end, [])]
+    seen = {}
+
+    best_score = 1e9
+    best_paths = defaultdict(list)
+    while todo:
+        score, pt, d, path = heapq.heappop(todo)
+        # print(f"Visiting {pt} with score {score} and direction {d}")
+        if seen.get((pt, d), 1e9) < score:
+            continue
+        seen[pt, d] = score
+
+        if pt == END:
+            best_score = min(best_score, score)
+            best_paths[score].append(path + [pt])
+
+        for dir in DIRS:
+            np = pt + dir
+            if dir == -d:
                 continue
-            if prev_node in seen:
-                continue
-            if (node, dir) not in distances:
-                continue
-            min_dist = min(distances.get((node, dir), [float("inf")]))
-            # If we had to turn we've already added the cost in
-            if prev_dir and prev_dir != dir:
-                min_dist += 1000
-            dists[prev_node] = min_dist
-        # If we hit a dead end, just break out
-        if not dists:
-            print(f"Dead end at {node}")
-            return []
-        print(f"Candidates for {node} {dists}")
-        # This is the minimum distance for any of the neighbors of the current node
-        min_dist = min(dists.values())
-        # Now look back through the neighbors to find the ones that have the minimum distance
-        found_paths = []
-        for prev_node, dist in dists.items():
-            seen.add(prev_node)
-            if prev_node == start:
-                print("Found start")
-                return path
-            this_path = path.copy()
-            this_path.append(prev_node)
-            new_paths = get_paths_from_node(prev_node, start, seen=seen, prev_dir=dir)
-            if not new_paths:
-                # This was a dead end! It has been marked as seen so start over from here
-                continue
-            this_path.extend(new_paths)
-            found_paths.append((dist, this_path))
-        if not found_paths:
-            return []
-        fp_dists = [fp[0] for fp in found_paths]
-        min_fp_dist = min(fp_dists)
-        min_paths = [fp[1] for fp in found_paths if fp[0] == min_fp_dist]
-        # Flatten list of lists
-        min_paths = [item for sublist in min_paths for item in sublist]
-        return min_paths
 
-    paths = get_paths_from_node(end, start, seen=seen)
-    pr(len(set(paths)))
+            if dir == d and BOARD.get(np) == EMPTY:
+                heapq.heappush(todo, (score + 1, np, dir, [pt] + path))
+            elif BOARD.get(np) == EMPTY:
+                heapq.heappush(todo, (score + 1001, np, dir, [pt] + path))
 
-    def get_cost_from_path(path):
-        cost = 0
-        prev_d = "R"
-        for i in range(1, len(path)):
-            r1, c1 = path[i - 1]
-            r2, c2 = path[i]
-            d = (r2 - r1, c2 - c1)
-            cost += 1
-            if prev_d is not None and d != prev_d:
-                cost += 1000
-            #     print(f"Turned at node {r1, c1} - {r2, c2} {cost:,}")
-            # else:
-            #     print(f"No turn at node {r1, c1} - {r2, c2} {cost:,}")
-            prev_d = d
-        return cost
+    min_path_pts = set()
+    for path in best_paths[best_score]:
+        min_path_pts.update(set(path))
 
-    # pr(get_cost_from_path(min_path))
-
-    def print_path(grid, path, marker="[red]O[/red]"):
-        grid = deepcopy(grid)
-        for r, c in path:
-            grid[c][r] = marker
-        for row in grid:
-            print("".join(row))
-
-    print_path(grid, paths)
-
-    # Part 2
-    # How many tiles are a part of at least one of the best paths
-    # Luckily we've already got all the distances from the start to all the nodes
+    # Part 1 solution for validation
+    pr(best_score)
+    pr(len(min_path_pts))
