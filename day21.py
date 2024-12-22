5f45e4c9e123ecd7ce0929ed3f29bb5e3b076db0
Rob Harrigan <rob.harrigan89@gmail.com>
2024-12-22 00:11:50 -0600

    Days 21 and 22

---
+++ day21.py
@@ -0,0 +1,140 @@
+import re
+import sys
+from functools import cache, lru_cache
+
+import networkx as nx
+from rich import print
+from tqdm import tqdm
+
+from aoc.grid import Point
+from aoc.pr import pr
+
+sys.setrecursionlimit(10**9)
+CACHE = {}
+DIRS = {
+    "^": Point(0, -1),
+    "v": Point(0, 1),
+    "<": Point(-1, 0),
+    ">": Point(1, 0),
+}
+DIRS_INVERSE = {
+    Point(0, -1): "^",
+    Point(0, 1): "v",
+    Point(-1, 0): "<",
+    Point(1, 0): ">",
+}
+
+
+def build_gd(grid):
+    grid_dict = {}
+    for j, row in enumerate(grid):
+        for i, val in enumerate(row):
+            if val is None:
+                continue
+            grid_dict[val] = Point(i, j)
+
+    return grid_dict
+
+
+def play_buttons(button_str, grid_dict):
+    gdr = {v: k for k, v in grid_dict.items()}
+    start = grid_dict["A"]
+    # print(f"Starting at {start}")
+    out = ""
+    curr_loc = start
+    for char in button_str:
+        if char == "A":
+            out += gdr[curr_loc]
+        else:
+            curr_loc += DIRS[char]
+    return out
+
+
+@cache
+def get_pos_opts(pos, newpos, depth, partial_row):
+    if pos.x < newpos.x:
+        h = ">" * (newpos.x - pos.x)
+    elif pos.x > newpos.x:
+        h = "<" * (pos.x - newpos.x)
+    else:
+        h = ""
+    if pos.y < newpos.y:
+        v = "v" * (newpos.y - pos.y)
+    elif pos.y > newpos.y:
+        v = "^" * (pos.y - newpos.y)
+    else:
+        v = ""
+
+    if h == "":
+        opts = [v]
+    elif v == "":
+        opts = [h]
+    elif pos.x == 0 and newpos.y == partial_row:
+        opts = [h + v]
+    elif newpos.x == 0 and pos.y == partial_row:
+        opts = [v + h]
+    else:
+        opts = [h + v, v + h]
+
+    def bestopts():
+        for i in opts:
+            yield find_best(i + "A", depth - 1, True)
+
+    return min(bestopts())
+
+
+if __name__ == "__main__":
+    day_no = int(re.search(r"day(\d+).py", __file__).group(1))
+    print(f"Starting Day {day_no}")
+    data = """
+129A
+540A
+789A
+596A
+582A
+""".strip().splitlines()
+    sample = """
+029A
+980A
+179A
+456A
+379A
+""".strip().splitlines()
+    # Comment out this line to use actual data
+    # data = sample
+
+    grid = [["7", "8", "9"], ["4", "5", "6"], ["1", "2", "3"], [None, "0", "A"]]
+    gdn = build_gd(grid)
+    grid2 = [
+        [None, "^", "A"],
+        ["<", "v", ">"],
+    ]
+    gdd = build_gd(grid2)
+
+    def find_best(code, depth, is_directional):
+        # print(f"Finding best for {code} with depth {depth} and is_directional {is_directional}")
+        if depth <= 0:
+            return len(code)
+        res = 0
+        grid = gdd if is_directional else gdn
+        partial_row = 0 if is_directional else 3
+        start = grid["A"]
+        for c in code:
+            end = grid[c]
+            res += get_pos_opts(start, end, depth, partial_row)
+            start = end
+        return res
+
+    def get_complexity(data, depth):
+        c = 0
+        for line in data:
+            ans = find_best(line, depth, False)
+            c += ans * int(re.search(r"(\d+)", line).group(1))
+        return c
+
+    # Find the length of the shortest sequence and the numeric part of the code and multiply
+    print("Part 1")
+    pr(get_complexity(data, 3))
+
+    print("Part 2")
+    pr(get_complexity(data, 26))
---
+++ day22.py
@@ -0,0 +1,112 @@
+import itertools
+import re
+import sys
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+
+from rich import print
+from tqdm import tqdm
+
+from aoc.files import readlines
+from aoc.pr import pr
+
+sys.setrecursionlimit(10**6)
+
+
+def evolve(secret):
+    # mix = bitwise XOR, prune = modulo 16777216
+    # The secret evolves by multiplying by 64, mix then prune
+    # Divide by 32, mix and prune
+    # multiply by 2048, mix and prune
+    step1 = secret * 64
+    secret ^= step1
+    secret %= 16777216
+    step2 = secret // 32
+    secret ^= step2
+    secret %= 16777216
+    step3 = secret * 2048
+    secret ^= step3
+    secret %= 16777216
+    return secret
+
+
+if __name__ == "__main__":
+    day_no = int(re.search(r"day(\d+).py", __file__).group(1))
+    print(f"Starting Day {day_no}")
+    data = readlines(day_no)
+    sample = """
+1
+2
+3
+2024
+""".strip().splitlines()
+    # Comment out this line to use actual data
+    # data = sample
+
+    # Confirm evolution works
+    # secret = 123
+    # for _ in range(10):
+    #     secret = evolve(secret)
+    #     pr(secret)
+
+    # Part 1
+    tot = 0
+    for secret in data:
+        secret = int(secret)
+        for _ in range(2000):
+            secret = evolve(secret)
+        tot += secret
+    pr(tot)
+
+    # Part 2
+    # Price is the ones digit (mod 10)
+    # Monkeys look for changes in price
+    # We need a single sequence of 4 prices for each secret that says when to sell
+    history = defaultdict(list)
+    sales_lookup = defaultdict(dict)
+    for secret in data:
+        secret = int(secret)
+        initial = secret
+        last = secret % 10
+        last4 = deque(maxlen=4)
+        # print(f"Price {last}")
+        for _ in range(2_000):
+            secret = evolve(secret)
+            price = secret % 10
+            delta = price - last
+            last = price
+            last4.append(delta)
+            last4_tuple = tuple(last4.copy())
+            history[initial].append((price, last4_tuple))
+            if len(last4) != 4:
+                continue
+            if last4_tuple in sales_lookup[initial]:
+                continue
+            sales_lookup[initial][tuple(last4.copy())] = price
+            # print(f"Price {price} Delta: {delta}")
+
+    # Now sum up all of the prices for all of the numbers, What is the most bananas we can get?
+    print("Part 2")
+    sequences = set(v[1] for lt in history.values() for v in lt)
+
+    def get_sale_price(sales_list, seq):
+        for sale in sales_list:
+            if sale[1] == seq:
+                return sale[0]
+        return None
+
+    def get_sale_price_fast(initial_secret, seq):
+        return sales_lookup[initial_secret].get(seq)
+
+    m = 0
+    sales = defaultdict(int)
+    for seq in tqdm(sequences):
+        for k, v in history.items():
+            price = get_sale_price_fast(k, seq)
+            # if seq == (-1, 1, 0, 0):
+            #     print(f"For {k} price is {price}")
+            if price is not None:
+                sales[seq] += price
+
+    pr(max(sales, key=sales.get))
+    pr(max(sales.values()))
